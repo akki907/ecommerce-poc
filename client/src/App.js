ff2ad281ddea70cc13a1f6855309adb4e806f465
akki907 <ak.akki907@gmail.com>
2020-10-31 09:58:31 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <main>intial setup</main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
